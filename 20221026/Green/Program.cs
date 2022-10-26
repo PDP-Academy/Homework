d55b210b8f5758605c1a10f6ff5b88c9a62e06f3
Diyorbek25 <diyorbek2506@gmail.com>
2022-10-26 17:52:54 +0500

    json deserialized

--- 20221026/Green/Program.cs
+++ 20221026/Green/Program.cs
@@ -2,14 +2,29 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Net.Http;
+using Green;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
         string url = "https://www.omdbapi.com/?i=tt3896198&apikey=";
+        
         string myKey = "ca272312";
 
+        HttpClient client = new HttpClient();
+        client.BaseAddress = new Uri(url + myKey);
 
+        var response = client.GetAsync(url + myKey).Result;
+        var contentString = response.Content.ReadAsStringAsync().Result;
+
+        var option = new JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true
+        };
+        var movie = JsonSerializer.Deserialize<Movie>(contentString, option);
+        Console.WriteLine(movie.Language);
+        Console.WriteLine(movie.Title);
 
     }
 }
