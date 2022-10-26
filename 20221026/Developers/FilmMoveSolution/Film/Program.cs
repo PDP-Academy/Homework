6cc24dd2861c2e4d643394ff735e07cf8ca2fc2a
Sardorbek <sardorbe2015@gmail.com>
2022-10-26 22:24:33 +0500

    Sardorbek

--- 20221026/Developers/FilmMoveSolution/Film/Program.cs
+++ 20221026/Developers/FilmMoveSolution/Film/Program.cs
@@ -1,2 +1,25 @@
 ﻿Meneger meneger = new Meneger();
-System.Console.WriteLine(meneger.Search(""));
+Console.WriteLine(meneger.Search(""));
+
+
+
+static void SHow(List<string> list)
+{
+    for (int i = 0; i < list.Count; i++)
+    {
+        Console.WriteLine(list[i]);
+        if (i + 1 % 10 == 0)
+        {
+            Console.WriteLine("<- Oraqaga || Oldinga ->");
+            int key = (int)Console.ReadKey().Key;
+            if (key == 39 && i != list.Count - 1)
+            {
+                continue;
+            }
+            else if (key == 37 && i != 0)
+            {
+                i -= 10;
+            }
+        }
+    }
+}
