e22e7089dcce05e280ad2483ffdcc5a3d949f190
Diyorbek25 <diyorbek2506@gmail.com>
2022-10-26 17:37:26 +0500

    Rating va Movie class lari qo'shildi

--- 20221026/Green/Program.cs
+++ 20221026/Green/Program.cs
@@ -5,5 +5,7 @@
         string url = "https://www.omdbapi.com/?i=tt3896198&apikey=";
         string myKey = "ca272312";
 
+
+
     }
 }
---
+++ 20221026/Green/Rating.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Green;
+
+internal class Rating
+{
+    public string Source { get; set; }
+    public string Value { get; set; }
+}
