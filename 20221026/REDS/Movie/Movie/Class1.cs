ea5ee803183f89086d3476d709b0ec772bca6ec3
Muxammadyor Kenjayev <muxammadyorkenjayev21@gmail.com>
2022-10-26 18:09:24 +0500

    Classlar yaratildi to'liqmas

---
+++ 20221026/REDS/Movie/Movie/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace Movie
+{
+
+    public class Root
+    {
+        public List<Search> Search { get; set; }
+        public string TotalResults { get; set; }
+        public string Response { get; set; }
+    }
+
+}
--- 20221026/REDS/Movie/Movie/Program.cs
+++ 20221026/REDS/Movie/Movie/Program.cs
@@ -4,8 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+            string s=Console.ReadLine();
+            int i=int.Parse(Console.ReadLine());
+            string url = $"https://omdbapi.com/?s={s}&page={i}&apikey=d8c25eb8";
+
+           
+            HttpClient client = new HttpClient();
+
+            var response = client.GetAsync(url).Result;
+            var json=response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(json);
+
         }
+        
+
+        
+
     }
 }
