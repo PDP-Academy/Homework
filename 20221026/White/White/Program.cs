7f5fe20fbfac0f148c06295ad1337dcc8434de09
Abdumannon <0605AbMu@gmail.com>
2022-10-26 15:02:03 +0500

    Created new branch for white group

---
+++ 20221026/White/White/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace White
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is White group project");
+        }
+    }
+}
