fb7ca6899e0db009c44cca67ba9a68414d6ba930
Shohruz <shohruzbobobekov505@gmail.com>
2022-10-26 20:35:27 +0500

    commit

--- 20221026/REDS/Movie/Movie/Program.cs
+++ 20221026/REDS/Movie/Movie/Program.cs
@@ -4,84 +4,15 @@
     {
         static void Main(string[] args)
         {
-
-            string s = Console.ReadLine();
-            int i = int.Parse(Console.ReadLine());
-            string url = $"https://omdbapi.com/?s={s}&page={i}&apikey=d8c25eb8";
-
-           
-            HttpClient client = new HttpClient();
-
-            var response = client.GetAsync(url).Result;
-            var json = response.Content.ReadAsStringAsync().Result;
-            
-            while(true)
+            while (true)
             {
-                Menu();
-                int choose = int.Parse(Console.ReadLine());
-                switch(choose)
-                {
-                    case 0:
-                        Console.Clear();                       
-                        return;
-                    case 1:
-                        Console.Clear();
-                        GetList(); 
-                        break;
-                    case 2:
-                        Console.Clear();
-                        MenuTitle();
-                        int choose2 = int.Parse(Console.ReadLine());
-                        switch(choose2)
-                        {
-                            case 0:
-                                Console.Clear();
-                                Thread.Sleep(1000);
-                                return;
-                            case 1:
-                                Console.Clear();
-                                ViewMovies();
-                                break;
-                            case 2:
-                                Console.Clear();
-                                ViewSeries();
-                                break;
-                            case 3:
-                                Console.Clear();
-                                ViewEpisode();
-                                break;
-                        }
-                        break;
-                }
-            }
+                Console.WriteLine();
         }
-        static void ViewEpisode()
-        {
+
 
         }
-        static void ViewSeries()
-        {
 
-        }
-        static void ViewMovies()
-        {
 
-        }
-        static void GetList()
-        {
-            int page = 1;
-            Console.Write("     Search \n Key: ");
-            string key=Console.ReadLine();
-            string url = $"https://omdbapi.com/?s={key}&page={page}&apikey=d8c25eb8";
-
-        }
-        static void Menu()
-        {
-            Console.WriteLine("1: Sort By Title");
-            Console.WriteLine("2: Sort By Type");
-            Console.WriteLine("0: Exit the Program");
-            Console.Write("select a page: ");
-        }
         static void MenuTitle()
         {
             Console.WriteLine("1: View list of Movies"); // kinolar ro'yhatini ko'rish
