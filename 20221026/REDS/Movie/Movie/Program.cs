3cb819be1f9918a484312ae9f27f8d3e397c8ab0
Javohir Jumanov <jumanovjavohir49@gmail.com>
2022-10-26 19:39:57 +0500

    Paka

--- 20221026/REDS/Movie/Movie/Program.cs
+++ 20221026/REDS/Movie/Movie/Program.cs
@@ -4,27 +4,70 @@
     {
         static void Main(string[] args)
         {
-            
 
-            string s=Console.ReadLine();
-            int i=int.Parse(Console.ReadLine());
-            string url = $"https://omdbapi.com/?s={s}&apikey=d8c25eb8";
+            string s = Console.ReadLine();
+            int i = int.Parse(Console.ReadLine());
+            string url = $"https://omdbapi.com/?s={s}&page={i}&apikey=d8c25eb8";
 
            
             HttpClient client = new HttpClient();
 
             var response = client.GetAsync(url).Result;
             var json = response.Content.ReadAsStringAsync().Result;
-            Console.WriteLine(json);
+            
+            while(true)
+            {
+                Menu();
+                int choose = int.Parse(Console.ReadLine());
+                switch(choose)
+                {
+                    case 0:
+                        Console.Clear();
+                        Thread.Sleep(1000);
+                        return;
+                    case 1:
+                        Console.Clear();
+                        SortedByType(); 
+                        break;
+                    case 2:
+                        Console.Clear();
+                        MenuTitle();
+                        int choose2 = int.Parse(Console.ReadLine());
+                        switch(choose2)
+                        {
+                            case 0:
+                                Console.Clear();
+                                Thread.Sleep(1000);
+                                return;
+                            case 1:
+                                Console.Clear();
 
+                                break;
+
+                        }
+                        break;
+                }
+            }
+        }
+
+        static void SortedByType()
+        {
+            
         }
         static void Menu()
         {
-            Console.WriteLine("Type buyich saralash");
-            Console.WriteLine("Title buyic");
+            Console.WriteLine("1: Sort By Type");
+            Console.WriteLine("2: Sort By Title");
+            Console.WriteLine("0: Exit the Program");
+            Console.Write("select a page: ");
         }
-
-        
-
+        static void MenuTitle()
+        {
+            Console.WriteLine("1: View list of Movies"); // kinolar ro'yhatini ko'rish
+            Console.WriteLine("2: View list of Series"); // seriallar ro'yhatini ko'rish
+            Console.WriteLine("3: View list of Episode"); // episodelar ro'yhatini ko'rish
+            Console.WriteLine("0: Exit the Program");
+            Console.Write("select a page: ");
+        }
     }
 }
