98b066d0edf94dfb65e5f345164fb8d673900bb2
Zuhriddin <zayniddinovzuhriddin99@gmail.com>
2022-10-27 11:58:06 +0500

    finish

--- 20221026/Developers/FilmMoveSolution/Film/Program.cs
+++ 20221026/Developers/FilmMoveSolution/Film/Program.cs
@@ -1,8 +1,13 @@
 ﻿Meneger meneger = new Meneger();
 MainPage mainPage = new MainPage();
+bosh:
 string path = mainPage.Filter();;
 var temp = meneger.Search(path);
-int k = int.Parse(temp.totalResults);
+int k =0;
+if(temp.totalResults!=null && temp.totalResults!="")
+k = int.Parse(temp.totalResults);
+else
+System.Console.WriteLine(" Afsuski kino topilmadi ");
 if(k % 10 == 0)
 k = k / 10;
 else
@@ -17,7 +22,10 @@ for (int i = 1; i <= k; i++)
         System.Console.WriteLine(temp.Search[j]);
     }
     System.Console.WriteLine($"<- {i}/{k} ->");
+    System.Console.WriteLine(" chiqish uchun Enter");
     char ch = (char)(Console.ReadKey().Key);
+    if((int)ch == 13)
+    break;
     if((int)ch == 39)
         if(i < k)
         continue;
@@ -31,3 +39,6 @@ for (int i = 1; i <= k; i++)
     else
         i--;
 }
+System.Console.WriteLine(" Yana boshqa film qidirib ko'rasizmi? Yes/No");
+if('Y' == (char)(Console.ReadKey().Key))
+goto bosh;
