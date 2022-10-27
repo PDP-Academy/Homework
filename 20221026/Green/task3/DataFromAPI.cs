04f7f0f5a35b8d33105c733473a963ad2c24c44d
Nurilloh <nurilloh6@gmail.com>
2022-10-27 22:01:01 +0500

    updated

---
+++ 20221026/Green/task3/DataFromAPI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace task3
+{
+    public class DataFromAPI
+    {
+        public DataFromAPI(List<Country> country, string name)
+        {
+            this.Name = name;
+            this.Country = country;
+        }
+
+        public List<Country> Country { get; set; }
+        public string Name { get; set; }
+    }
+}
+
--- 20221026/Green/task3/Program.cs
+++ 20221026/Green/task3/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+namespace task3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        string name = "test";
+        string api = "https://api.nationalize.io?";
+
+        HttpClient client = new HttpClient();
+        var response = client.GetAsync(api + name).Result;
+        string content = response.Content.ReadAsStringAsync().Result;
+
+
+    }
+}
