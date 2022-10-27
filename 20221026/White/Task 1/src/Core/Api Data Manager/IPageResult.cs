a55d00f79a6e1ec0bffbd3839c8f2402cc7cb953
= <=>
2022-10-27 10:37:59 +0500

    IPageResult yaratdim

---
+++ 20221026/White/Task 1/src/Core/Api Data Manager/IPageResult.cs
@@ -0,0 +1,21 @@
+﻿
+namespace White.Core
+{
+    internal interface IPageResult
+    {
+        /// <summary>
+        /// Apidan kelgan resultlar soni
+        /// </summary>
+        public string TotalResults { get; set; }
+        /// <summary>
+        /// Apidan kelgan result holati
+        /// </summary>
+
+        public string Response { get; set; }
+        /// <summary>
+        /// Resultlari listi
+        /// </summary>
+
+        public List<IResult> Results { get; set; }
+    }
+}
--- 20221026/White/Task 1/src/Core/Api Data Manager/Movie.cs
+++ 20221026/White/Task 1/src/Core/Api Data Manager/Movie.cs
@@ -5,9 +5,9 @@ namespace White.Core
     public class Movie : IResult
     {
         public string Title { get; set; }
-        public string Message { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string ImdbId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string Type { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string Poster { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Year { get ; set ; }
+        public string ImdbId { get ; set ; }
+        public string Type { get ; set; }
+        public string Poster { get ; set ; }
     }
 }
