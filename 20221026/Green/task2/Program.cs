c5f5bb1188e5a8365450e9f256e53d139df25dd9
Diyorbek25 <diyorbek2506@gmail.com>
2022-10-27 14:23:51 +0500

    update

--- 20221026/Green/task2/Program.cs
+++ 20221026/Green/task2/Program.cs
@@ -9,121 +9,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        SearchByName();
-        // WallStreetJournal();
+
+
         Menu1();
-    }
 
 
-
-
-    static void SearchByName()
-    {
-        Console.Write("Value kiriting: ");
-        string name = Console.ReadLine();
-        string url = $@"https://newsapi.org/v2/everything?q={name}&apiKey=e20df51e594a4b7daae76ca0ce171f4e";
-        HttpClient client = new HttpClient();
-        var response = client.GetAsync(url).Result;
-        var content = response.Content.ReadAsStringAsync().Result;
-        var json = JsonSerializer.Deserialize<Root>(content, new JsonSerializerOptions() {PropertyNameCaseInsensitive = true });
-        int pagenation = 10;
-        int i = 0;
-        Console.WriteLine(url);
-        while (true)
-        {
-            Console.Clear();
-            for (; i < pagenation && i < json.totalResults; i++)
-            {
-                Console.WriteLine($"{i + 1}. {json.articles[i].title}");
-            }
-            Console.WriteLine($@"<<{i / 10} - {json.totalResults / 10}>>");
-            var key = Console.ReadKey().Key;
-            if (pagenation == 1 && key == ConsoleKey.LeftArrow)
-                continue;
-            if (pagenation == json.totalResults / 10 && ConsoleKey.RightArrow == key)
-                continue;
-            if (ConsoleKey.LeftArrow == key)
-            {
-                i -= 20;
-                pagenation -= 10;
-            }
-            if (ConsoleKey.RightArrow == key)
-            {
-                i = pagenation;
-                pagenation += 10;
-            }
-
-            if (ConsoleKey.Escape == key)
-                return;
-            
-        }
-
-
-        HttpClient client = new HttpClient();
-
-        var response = client.GetAsync(url).Result;
-        var content = response.Content.ReadAsStringAsync().Result;
-
-        var option = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
-
-        var json = JsonSerializer.Deserialize<Root>(content, option);
-        int pagenation = 10;
-        int i = 0;
-        Console.WriteLine(url);
-
-        while (true)
-        {
-            Console.Clear();
-
-            for (; i < pagenation && i < json.totalResults; i++)
-            {
-                Console.WriteLine($"{i + 1}. {json.articles[i].title}");
-            }
-            Console.WriteLine($@"<<{pagenation / 10} - {json.totalResults / 10}>>");
-            var key = Console.ReadKey().Key;
-            if (pagenation == 1 && key == ConsoleKey.LeftArrow)
-                continue;
-            if (pagenation == json.totalResults / 10 && ConsoleKey.RightArrow == key)
-
-            HttpClient client = new HttpClient();
-            
-            var response = client.GetAsync(url).Result;
-            var content = response.Content.ReadAsStringAsync().Result;
-            var json = JsonSerializer.Deserialize<Root>(content, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
-            
-            int pagenation = 10;
-            int i = 0;
-            Console.WriteLine(url);
-            while (true)
-            {
-                Console.Clear();
-                for (; i < pagenation && i < json.totalResults; i++)
-                {
-                    Console.WriteLine($"{i + 1}. {json.articles[i].title}");
-                }
-                Console.WriteLine($@"<<{pagenation / 10} - {json.totalResults / 10}>>");
-                var key = Console.ReadKey().Key;
-                if (pagenation == 1 && key == ConsoleKey.LeftArrow)
-                    continue;
-                if (pagenation == json.totalResults / 10 && ConsoleKey.RightArrow == key)
-                    continue;
-                if (ConsoleKey.LeftArrow == key)
-                {
-                    i -= 20;
-                    pagenation -= 10;
-                }
-                if (ConsoleKey.RightArrow == key)
-                {
-                    i = pagenation;
-                    pagenation += 10;
-                }
-
-                if (ConsoleKey.Escape == key)
-                    return;
-
-            }
-
-        }
 
     }
 
@@ -141,15 +31,17 @@ class Program
         switch (change)
         {
             case 1:
+
                 {
                 }
                 break;
             case 2:
                 {
                     ByCategory();
-                    Menu1();
+                    
                 }
                 break;
+
             case 3:
                 {
 
@@ -330,5 +222,4 @@ class Program
             Console.Clear();
         }
     }
-
 }
