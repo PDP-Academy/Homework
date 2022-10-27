534439e48a53e3ce32fbc0a9758e7f57600b31fc
Abdumannon <0605AbMu@gmail.com>
2022-10-27 09:32:09 +0500

    Fixed project structure

--- 20221026/White/Task 1/src/Core/Api Data Manager/ApiDataManager.cs
+++ 20221026/White/Task 1/src/Core/Api Data Manager/ApiDataManager.cs
@@ -4,7 +4,7 @@ internal class ApiDataManager : IApiDataManager
 {
 
     // /<Summary>O'zida API uchun key saqlaydi</Summary>
-    // private string ApiKey { get; }
+    private string ApiKey { get; }
 
     ///<Summary>API URL base</Summary>
     private string ApiUrl { get => "http://img.omdbapi.com/"; }
--- 20221026/White/Task 1/src/Core/Api Data Manager/IApiDataManger.cs
+++ 20221026/White/Task 1/src/Core/Api Data Manager/IApiDataManger.cs
@@ -2,10 +2,5 @@ namespace White.Core;
 
 internal interface IApiDataManager
 {
-    ///<Summary>O'zida API uchun key saqlaydi</Summary>
-    public string ApiKey { get; }
-    ///<Summary>Apidan ma'lumot olib beradi</Summary>
-    public List<Object> GetDataFromApi(string url);
-    ///<Summary>Title bo'yicha qidirib ma'lumotlarni uzatadi</Summary>
     public List<Object> SearchDataByTitle(string title);
 }
