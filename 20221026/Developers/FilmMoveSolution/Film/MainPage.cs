e6d79f6187d31a5590a4721b115ebfe4d58ab4af
Zuhriddin <zayniddinovzuhriddin99@gmail.com>
2022-10-27 11:35:11 +0500

    dd

--- 20221026/Developers/FilmMoveSolution/Film/MainPage.cs
+++ 20221026/Developers/FilmMoveSolution/Film/MainPage.cs
@@ -60,7 +60,6 @@ tittle:
     {
         Inputs();
         string str = "";
-        if(s != null)
             str = str + "&" + $"s={s}";
         if(y != null)
             str = str + "&" + $"y={y}";
