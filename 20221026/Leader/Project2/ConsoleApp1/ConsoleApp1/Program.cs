9d31304ffdfeaf081c110f00de44f67007b57511
TohirjonIbnAlisher <ibnalisher7379@gmail.com>
2022-10-27 23:11:30 +0500

    changed

---
+++ 20221026/Leader/Project2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("1.So‘nggi 4 yil ichida 80 000 dan ortiq turli xil katta" +
+                " va kichik manbalar tomonidan chop etilgan har bir maqolani qidiring.\n" +
+                "2."
+        }
+    }
+}
