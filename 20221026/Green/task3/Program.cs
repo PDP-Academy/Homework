a2a7f63a57759b75fe9b041422c40eab2bef81ae
Diyorbek25 <diyorbek2506@gmail.com>
2022-10-27 23:42:12 +0500

    Update

--- 20221026/Green/task3/Program.cs
+++ 20221026/Green/task3/Program.cs
@@ -1,23 +1,58 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
+using System.Text.Json;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace task3;
 
 class Program
 {
+    static string api = "https://api.nationalize.io?name=";
     static void Main(string[] args)
     {
-        string path = new Uri(new Uri(System.AppDomain.CurrentDomain.BaseDirectory), "data.txt").AbsolutePath;
-        string name = "test";
-        string api = "https://api.nationalize.io?";
-        Console.WriteLine(path);
+        var nationalityOfNames = new List<Country>();
+        var names = GetNames();
 
+        for (int i = 0; i < names.Count; i++)
+        {
+            nationalityOfNames.Add(PredictNationalityOfName(names[i]));
+
+            var country = nationalityOfNames[i];
+            Console.WriteLine($"Country Id: {country.Country_id}");
+            Console.WriteLine($"Probability: {country.Probability}");
+        }
+       
+
+    }
+
+    static Country PredictNationalityOfName(string name)
+    {
         HttpClient client = new HttpClient();
         var response = client.GetAsync(api + name).Result;
         string content = response.Content.ReadAsStringAsync().Result;
 
+        var option = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
 
+        var dataFromApi = JsonSerializer.Deserialize<DataFromAPI>(content, option);
+        var countries = dataFromApi.Country;
+
+        return countries[0];
+    }
+
+    static List<string> GetNames()
+    {
+        var namesList = new List<string>();
+        string path = new Uri(new Uri(System.AppDomain.CurrentDomain.BaseDirectory), "names.txt").AbsolutePath;
+        Console.WriteLine(path);
+        using (StreamReader streamReader = new StreamReader(path))
+        {
+            while (!streamReader.EndOfStream)
+            {
+                namesList.Add(streamReader.ReadLine());
+            }
+        }
+
+        return namesList;
     }
 }
