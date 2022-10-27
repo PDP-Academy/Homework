c9c2cdd746143ed7f18d4c68a5ad7710c681c519
Nurilloh <nurilloh6@gmail.com>
2022-10-27 22:15:46 +0500

    updated

--- 20221026/Green/task3/Program.cs
+++ 20221026/Green/task3/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
+using static System.Net.Mime.MediaTypeNames;
+
 namespace task3;
 
 class Program
 {
     static void Main(string[] args)
     {
-        
+        string path = new Uri(new Uri(System.AppDomain.CurrentDomain.BaseDirectory), "data.txt").AbsolutePath;
         string name = "test";
         string api = "https://api.nationalize.io?";
+        Console.WriteLine(path);
 
         HttpClient client = new HttpClient();
         var response = client.GetAsync(api + name).Result;
