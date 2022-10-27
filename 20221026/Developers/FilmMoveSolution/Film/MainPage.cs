3e96416069b6832aea6d627f83af9e6746c60f8c
Khojiakbar <hamrakulov2002@mail.ru>
2022-10-27 11:57:04 +0500

    mainpage ok

--- 20221026/Developers/FilmMoveSolution/Film/MainPage.cs
+++ 20221026/Developers/FilmMoveSolution/Film/MainPage.cs
@@ -3,7 +3,7 @@ class MainPage
     public string s { get; set; } = null; // filmni nomi 
     public string y { get; set; } = null; // filmni yili
     public string i {get; set;} = null; // filmni IMBDmi
-    
+    public string type {get; set;} = null;
     public void Inputs()
     {
 tittle: 
@@ -17,6 +17,40 @@ tittle:
         else if(answer == "Yoq" || answer == "yoq")
         {
         }
+        System.Console.WriteLine("Typeni kiritasizmi?   Xa \\ Yoq  ");
+        answer = Console.ReadLine();
+        if(answer == "xa" || answer == "Xa")
+        {
+            System.Console.WriteLine("1) movie");
+            System.Console.WriteLine("2) series");
+            System.Console.WriteLine("3) episodes");
+            System.Console.WriteLine("4) game");
+
+            System.Console.Write("Typini raqam bilan kiriting: ");
+            int input = int.Parse(Console.ReadLine());
+boshidan:   switch(input)
+            {
+                case 1:
+                    type = "movie";
+                    break;
+                case 2:
+                    type = "series";
+                    break;
+                case 3:
+                    type = "episodes";
+                    break;
+                case 4:
+                    type = "game";
+                    break;
+                default:
+                    System.Console.WriteLine("Xato boshidan kriting");
+                    goto boshidan;
+                    break;
+            }
+
+
+        }
+
 //         System.Console.Write("Yilni kiritasizmi?  Xa \\ Yoq ");
 //         answer = Console.ReadLine();
 //         }
@@ -55,6 +89,8 @@ tittle:
         Inputs();
         string str = "";
             str = str + "&" + $"s={s}";
+        if(type != null)
+            str = str + "&" + $"type={type}";
         // if(y != null)
         //     str = str + "&" + $"y={y}";
         // if(i != null)
