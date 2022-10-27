b96c09522016d023ada621edeb92cd81202cff18
Abdumannon <0605AbMu@gmail.com>
2022-10-27 09:28:45 +0500

    Created IResult

--- 20221026/White/Task 1/src/Core/Api Data Manager/ApiDataManager.cs
+++ 20221026/White/Task 1/src/Core/Api Data Manager/ApiDataManager.cs
@@ -3,8 +3,8 @@ namespace White.Core;
 internal class ApiDataManager : IApiDataManager
 {
 
-    ///<Summary>O'zida API uchun key saqlaydi</Summary>
-    private string ApiKey { get; }
+    // /<Summary>O'zida API uchun key saqlaydi</Summary>
+    // private string ApiKey { get; }
 
     ///<Summary>API URL base</Summary>
     private string ApiUrl { get => "http://img.omdbapi.com/"; }
