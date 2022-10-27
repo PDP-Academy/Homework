7b7aa99a751c4dfd2575cae10b92cf11f8a8b2ce
muxam <muxammadyorkenjayev21@gmail.com>
2022-10-27 08:36:31 +0500

    11

--- 20221026/REDS/Movie/Movie/Movie.cs
+++ 20221026/REDS/Movie/Movie/Movie.cs
@@ -1,14 +1,18 @@
-﻿namespace Movies
+﻿using System.Text.Json.Serialization;
+
+namespace Movies
 {
 
     public class Search
     {
-        public List<Movie> Searc { get; set; }
+        [JsonPropertyName("Search")]
+        public List<Movie> Movie { get; set; }
+
         public string TotalResults { get; set; }
         public string Response { get; set; }  
         public override string ToString()
         {
-            return $"InfoOfMovie:{Searc},Total results:{TotalResults},Response:{Response}"; 
+            return $"InfoOfMovie:,Total results:{TotalResults},Response:{Response}"; 
         }
 
     }
--- 20221026/REDS/Movie/Movie/Program.cs
+++ 20221026/REDS/Movie/Movie/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Movie
+﻿using Movies;
+using System.Text.Json;
+
+namespace Movie
 {
     internal class Program
     {
@@ -9,17 +12,32 @@
                 Console.Write("Filem nomi: ");
                 string nameFilm = Console.ReadLine();
                 Type();
-                string type = Console.ReadLine();
-                string url = $"https://omdbapi.com/?s={nameFilm}&type={type}&page=1&page=1&apikey=d8c25eb8";
-                int count=10;
+                int nameTypePosition = int.Parse(Console.ReadLine());
+                string nameType;
+                if (nameTypePosition == 1)
+                    nameType = "movie";
+                if (nameTypePosition == 2)
+                    nameType = "series";
+                else
+                    nameType = "episode";
+
+                
+                string url ;
+            
+                int count=100;
                 int pasition=1;
 
                 while(pasition<=count/10)
                 {
-                    url= $"https://omdbapi.com/?s={nameFilm}&type={type}&page={pasition}&page=1&apikey=d8c25eb8";
-                   // foreach ()
+                    url= $"https://omdbapi.com/?s={nameFilm}&type={nameType}&page={pasition}&apikey=d8c25eb8";
+                    Search search = GetSearchByUrl(url);
+                    foreach (var item in search.Movie)
+                    {
+                        Console.WriteLine(item);
+                    }
 
-                    var strelka= Console.ReadKey(false).Key; ;
+                    var strelka= Console.ReadKey(false).Key;
+                    Console.Clear();
                     switch( strelka)
                     {
                         case ConsoleKey.RightArrow:
@@ -32,33 +50,31 @@
                             if (pasition != 1)
                                 pasition--;
                             break;
-                            case
+                        case ConsoleKey.UpArrow:
+                            pasition = count / 10 + 1;
+                            break;
 
-                    }
-                    
+                    }                    
                 }
-
-
-
-
             }
-
-
         }
         
 
-
-        static void MenuTitle()
+        private static Search GetSearchByUrl(string url)
         {
-            Console.WriteLine("1: View list of Movies"); // kinolar ro'yhatini ko'rish
-            Console.WriteLine("2: View list of Series"); // seriallar ro'yhatini ko'rish
-            Console.WriteLine("3: View list of Episode"); // episodelar ro'yhatini ko'rish
-            Console.WriteLine("0: Exit the Program");
-            Console.Write("select a page: ");
+            HttpClient client = new HttpClient();
+            var response = client.GetAsync(url).Result;
+            var json = response.Content.ReadAsStringAsync().Result;
+            var search = JsonSerializer.Deserialize<Search>(json, new JsonSerializerOptions()
+            { 
+                PropertyNameCaseInsensitive=true
+            });
+            return search;
         }
+       
         static void Type()
         {
-            Console.WriteLine("movie , series, ");
+            Console.WriteLine("1.movie \n2.series \n3.episode");
             Console.Write("Tanlab yozing: ");
 
         }
