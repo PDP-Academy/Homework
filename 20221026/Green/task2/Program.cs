2d1d2afb42db27176df18c5bcb5a04f2af547aae
Elchin <elchinuralov77@gmail.com>
2022-10-27 11:20:16 -1200

    task2

--- 20221026/Green/task2/Program.cs
+++ 20221026/Green/task2/Program.cs
@@ -1,15 +1,68 @@
 ﻿using System;
+using System.Text;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 namespace task2;
 
 class Program
 {
     static void Main(string[] args)
     {
-
+        WallStreetJournal();
     }
 
     static void SearchByName(string name)
     {
 
+    }
+    static void WallStreetJournal()
+    {
+        System.Console.WriteLine("Oxirgi 6 oy ichida Wall Street Journal tomonidan chop etilgan barcha maqolalar,\nso'nggi birinchisi bo'yicha saralangan!");
+
+        string url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=e20df51e594a4b7daae76ca0ce171f4e";
+
+        HttpClient client = new HttpClient();
+
+        var response = client.GetAsync(url).Result;
+
+        string contentString = response.Content.ReadAsStringAsync().Result;
+
+        System.Console.WriteLine(contentString);
+
+        var option = new JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        var root = JsonSerializer.Deserialize<Root>(contentString, option);
+
+        int result = root.totalResults;
+
+        for (int i = 0; i < result;)
+        {
+            System.Console.WriteLine("Author: " + root.articles[i].author);
+            System.Console.WriteLine("content: " + root.articles[i].content);
+            System.Console.WriteLine("description: " + root.articles[i].description);
+            System.Console.WriteLine("publishedAt: " + root.articles[i].publishedAt);
+            System.Console.WriteLine("ID: " + root.articles[i].source.id);
+            System.Console.WriteLine("Name: " + root.articles[i].source.name);
+            System.Console.WriteLine("Title: " + root.articles[i].title);
+            System.Console.WriteLine("Url: " + root.articles[i].url);
+            System.Console.WriteLine("UrlToImage: " + root.articles[i].urlToImage);
+            System.Console.WriteLine();
+            System.Console.WriteLine($"{i + 1} / {result}");
+
+            var change = Console.ReadKey().Key;
+
+            if (change == ConsoleKey.RightArrow)
+                i++;
+            else if (change == ConsoleKey.LeftArrow && i != 0)
+                i--;
+            else if (change == ConsoleKey.Backspace)
+                return;
+
+
+        }
     }
 }
