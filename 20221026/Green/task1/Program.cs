32ff03c331bf28ac174be376609835261efb9841
Elchin <elchinuralov77@gmail.com>
2022-10-27 09:31:04 -1200

    yakunlandi

--- 20221026/Green/task1/Program.cs
+++ 20221026/Green/task1/Program.cs
@@ -10,7 +10,7 @@ internal class Program
     private static void Main(string[] args)
     {
         string url = "https://www.omdbapi.com/?t=Bn&&apikey=";
-        
+
         string myKey = "ca272312";
 
         HttpClient client = new HttpClient();
@@ -25,14 +25,14 @@ internal class Program
         };
 
         MainMenu();
-        
+
     }
 
     static void MainMenu()
     {
         System.Console.WriteLine();
         System.Console.WriteLine("1.Title bo'yicha qidirish");
-        System.Console.WriteLine("2.Toifa bo'yicha qidirish");
+        System.Console.WriteLine("2.Toifa bo'yicha saralash");
         System.Console.WriteLine();
         System.Console.Write("-->  ");
 
@@ -41,19 +41,22 @@ internal class Program
         switch (change)
         {
             case 1:
-            {
-                    SearchByTitle();       
-            } break;
+                {
+                    SearchByTitle();
+                }
+                break;
 
             case 2:
-            {
-
-            } break;
+                {
+                    SortedToType();
+                }
+                break;
 
             default:
-            {
+                {
 
-            } break;
+                }
+                break;
         }
     }
     static void SearchByTitle()
@@ -63,14 +66,14 @@ internal class Program
         string pageStr = "&page=";
         string myKey = "&apikey=ca272312";
         int index = 1;
-        
+
         IManagerAPI managerAPI = new ManagerAPI();
 
         Console.Write("title: ");
         title += Console.ReadLine();
         int pages = 10;
 
-        while (index <= pages/10 && index > 0)
+        while (index <= pages / 10 && index > 0)
         {
             string newUrl = url + title + pageStr + $"{index}" + myKey;
             string contentString = managerAPI.GetContentString(newUrl);
@@ -88,7 +91,7 @@ internal class Program
             {
                 Console.WriteLine("  Title: " + item.Title);
             }
-            Console.Write($"\t{index}/{pages/10}");
+            Console.Write($"\t{index}/{pages / 10}");
 
             var change = Console.ReadKey().Key;
 
@@ -107,16 +110,121 @@ internal class Program
         string type = "type=";
         string pageStr = "&page=";
         string myKey = "&apikey=ca272312";
+        int index = 1;
 
-        List<Movie> movies = new List<Movie>();
+        List<Movie> moviesSort = new List<Movie>();
+        List<Movie> series = new List<Movie>();
+        List<Movie> episode = new List<Movie>();
         IManagerAPI managerAPI = new ManagerAPI();
 
         Console.Write("title: ");
         title += Console.ReadLine();
+
+        string newUrl = url + title + pageStr + $"{index}" + myKey;
+        string contentString = managerAPI.GetContentString(newUrl);
+
+        var option = new JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true
+        };
+        var search = JsonSerializer.Deserialize<Search>(contentString, option);
+
+        var moviesList = search.Movies;
+
+        for (int i = 0; i < moviesList.Count; i++)
+        {
+            if (moviesList[i].Type == "movie")
+                moviesSort.Add(moviesList[i]);
+
+            else if (moviesList[i].Type == "series")
+                series.Add(moviesList[i]);
+
+            else if (moviesList[i].Type == "episode")
+                episode.Add(moviesList[i]);
+        }
+
         Console.WriteLine("1.Movie");
-        Console.WriteLine("2.");
+        Console.WriteLine("2.Series");
+        Console.WriteLine("3.Episode");
+        System.Console.WriteLine();
+        System.Console.WriteLine("-->  ");
+        int change = Convert.ToInt32(Console.ReadLine());
 
-        
+        switch (change)
+        {
+            case 1:
+                {
+                    MoviesSort(moviesSort);
+                }
+                break;
+            case 2:
+                {
+                    Series(series);
+                }
+                break;
+            case 3:
+                {
+                    Episode(episode);
+                }
+                break;
+            default: break;
+        }
+    }
+    static void MoviesSort(List<Movie> moviesSort)
+    {
+        for (int i = 0; i < moviesSort.Count;)
+        {
+            System.Console.WriteLine("Title: " + moviesSort[i].Title);
+            System.Console.WriteLine("Type: " + moviesSort[i].Type);
+            System.Console.WriteLine();
+            System.Console.WriteLine($"{i + 1} / {moviesSort.Count}");
 
+            var change = Console.ReadKey().Key;
+
+            if (change == ConsoleKey.RightArrow)
+                i++;
+            else if (change == ConsoleKey.LeftArrow && i != 0)
+                i--;
+
+            Console.Clear();
+        }
+    }
+    static void Series(List<Movie> series)
+    {
+        for (int i = 0; i < series.Count; i++)
+        {
+            System.Console.WriteLine("Title: " + series[i].Title);
+            System.Console.WriteLine("Type: " + series[i].Type);
+            System.Console.WriteLine();
+            System.Console.WriteLine($"{i + 1} / {series.Count}");
+
+            var change = Console.ReadKey().Key;
+
+            if (change == ConsoleKey.RightArrow)
+                i++;
+            else if (change == ConsoleKey.LeftArrow)
+                i--;
+
+            Console.Clear();
+        }
+    }
+    static void Episode(List<Movie> episode)
+    {
+        for (int i = 0; i < episode.Count; i++)
+        {
+            System.Console.WriteLine("Title: " + episode[i].Title);
+            System.Console.WriteLine("Type: " + episode[i].Type);
+            System.Console.WriteLine();
+            System.Console.WriteLine($"{i + 1} / {episode.Count}");
+
+            var change = Console.ReadKey().Key;
+
+            if (change == ConsoleKey.RightArrow)
+                i++;
+            else if (change == ConsoleKey.LeftArrow)
+                i--;
+
+            Console.Clear();
+        }
     }
 }
