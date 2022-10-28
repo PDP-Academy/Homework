ffe54e0c6d8d07ccd8164fc849de29a155ff533c
Diyorbek25 <diyorbek2506@gmail.com>
2022-10-28 19:06:00 +0500

    add first

---
+++ 20221026/Green/Algo/Diyorbek Algo/First/Person.cs
@@ -0,0 +1,8 @@
+
+namespace First
+{
+    public class Person
+    {
+        
+    }
+}
--- 20221026/Green/Algo/Diyorbek Algo/First/Program.cs
+++ 20221026/Green/Algo/Diyorbek Algo/First/Program.cs
@@ -2,6 +2,20 @@
 {
     private static void Main(string[] args)
     {
-        
+        // Largest Element in Array
+        int[] arr = new int[]{ 1, 2, 3, 1000, 5, 690 };
+
+        System.Console.WriteLine(Largest(arr));
+
+    }
+    static int Largest(int[] arr){
+
+        int max = int.MinValue;
+
+        for (int i = 0; i < arr.Length; i++) {
+            if (arr[i] > max)
+                max = arr[i];
+        }
+        return max;
     }
 }
