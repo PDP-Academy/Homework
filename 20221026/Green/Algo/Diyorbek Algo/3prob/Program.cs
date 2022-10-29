96bcb1bf5d17f3565908f1c19322ade6d894de5a
Diyorbek25 <diyorbek2506@gmail.com>
2022-10-29 14:59:28 +0500

    add 3 problem solution

---
+++ 20221026/Green/Algo/Diyorbek Algo/3prob/Program.cs
@@ -0,0 +1,39 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = { 1,2,1,1 };
+        LinkedList<int> list = new LinkedList<int>();
+        
+
+        Console.WriteLine(Check(nums));
+
+    }
+    static bool Check(int[] nums)
+    {
+        int len = nums.Length;
+        int min = int.MaxValue, x = 0;
+
+        for (int i = 0; i < len; i++)
+        {
+            if (min > nums[i])
+            {
+                min = nums[i];
+                x = i;
+            }
+            if (min == nums[i] && i > 0 && nums[i] != nums[i - 1])
+            {
+                min = nums[i];
+                x = i;
+            }
+        }
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[(i - 1 + x) % len] > nums[(i + x) % len])
+                return false;
+        }
+        return true;
+    }
+}
