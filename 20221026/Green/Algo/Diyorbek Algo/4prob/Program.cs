1bc1560d2242f6bcfd3077fc1f62c7914e94c47a
Diyorbek25 <diyorbek2506@gmail.com>
2022-10-29 15:07:30 +0500

    add 4 problem solution

---
+++ 20221026/Green/Algo/Diyorbek Algo/4prob/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arr = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+
+        Console.WriteLine(RemoveDuplicates(arr));
+    }
+    static int RemoveDuplicates(int[] nums)
+    {
+        int temp = 101, j = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != temp)
+            {
+                nums[j++] = nums[i];
+                temp = nums[i];
+            }
+        }
+        return j;
+    }
+}
