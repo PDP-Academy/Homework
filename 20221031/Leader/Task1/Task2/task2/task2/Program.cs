a8c9e01069afa6b8c365f6025d52bbf420e082c9
TohirjonIbnAlisher <ibnalisher7379@gmail.com>
2022-10-31 23:59:30 +0500

    created all requirements

---
+++ 20221031/Leader/Task1/Task2/task2/task2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RecursiveThread(10);
+        }
+
+        static void RecursiveThread(int num)
+        {
+            if(num == 0) return;    
+            Thread thread = new Thread(() => Print(num));
+            thread.Start();
+            thread.Join();
+
+            num--;
+
+            RecursiveThread(num);
+        }
+
+        static void Print(int num)
+        {
+            Console.WriteLine(num);
+        }
+
+
+    }
+}
